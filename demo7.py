eb4f3dbbf692dc90d33606bf9ca36d4f06b9b784
zhangming <550697141@qq.com>
2017-10-22 20:09:44 +0800

    初始化

---
+++ demo7.py
@@ -0,0 +1,15 @@
+# -*- coding:utf8 -*-
+from mxnet import gluon
+from mxnet import ndarray as nd
+
+
+# 获取数据
+def transform(data, label):
+    return data.astype('float32') / 255, label.astype('float32')
+
+
+mnist_train = gluon.data.vision.FashionMNIST(train=True, transform=transform)
+#  mnist_test = gluon.data.vision.FashionMNIST(train=False, transform=transform)
+print('你好')
+
+
